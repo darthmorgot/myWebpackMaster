e04154adce2272e795b3d10e287ce760bbbc07ea
darthmorgot <dartmorgot@yandex.ru>
2021-02-24 20:38:22 +1000

    Мелкие правки кода

--- webpack.config/webpack.common.js
+++ webpack.config/webpack.common.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.js')
+    main: path.resolve(__dirname, '../src/index.js')
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../dist'),
     filename: '[name].bundle.js'
   }
 };
