c85fec56649c2e06f5116b8a905765cb3e94cbde
darthmorgot <dartmorgot@yandex.ru>
2021-02-24 20:40:11 +1000

    Небольшие исправления в коде

--- webpack.config/webpack.config.js
+++ webpack.config/webpack.config.js
@@ -4,13 +4,13 @@ const common = require('./webpack.common.js');
 const dev = require('./webpack.dev.js');
 const prod = require('./webpack.prod.js');
 
-module.exports = env => {
-  switch (env) {
+module.exports = () => {
+  switch (process.env.NODE_ENV) {
     case 'development':
       return merge(common, dev);
     case 'production':
       return merge(common, prod);
     default:
-      throw new Error('No matching configuretion was found');
+      throw new Error('No matching configuration was found');
   }
 };
