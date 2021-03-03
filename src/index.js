e008fec10a3c27656707293e4f40f209995a14e8
darthmorgot <dartmorgot@yandex.ru>
2021-03-03 20:54:15 +1000

    Мелкие правки кода для теста

--- src/index.js
+++ src/index.js
@@ -13,3 +13,24 @@ console.log(test(2, 4));
 const testRow = 'This is test row';
 
 console.log(`Test: ${testRow}`);
+
+const y = 110;
+const f = test(3, 2);
+
+const func = (x, callback) => {
+  return Math.floor(x / callback(3, 2));
+};
+
+console.log(`${y} / ${f} = ${func(y, test)}`);
+
+class Game {
+  constructor() {
+    this.name = 'Master';
+  }
+
+  caller() {
+    return this.name;
+  }
+}
+const game = new Game();
+console.log(game.caller());
