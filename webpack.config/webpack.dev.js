c7a7eef3baa813d17f295505db38a562d191fc31
darthmorgot <dartmorgot@yandex.ru>
2021-03-04 21:10:23 +1000

    Добавил настройки devServer, подключил плагин webpack.HotModuleReplacementPlugin

--- webpack.config/webpack.dev.js
+++ webpack.config/webpack.dev.js
@@ -1,4 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
   mode: 'development',
-  devtool: 'eval-cheap-module-source-map'
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    port: 8080,
+    open: 'chrome',
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, '../dist'),
+    compress: true,
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
