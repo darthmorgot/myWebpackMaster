7246e61a1897b171fea9cb384bc377dc16a8aacc
darthmorgot <dartmorgot@yandex.ru>
2021-03-06 22:09:38 +1000

    Подключил mini-css-extract-plugin, настроил обработку файлов для прода

--- webpack.config/webpack.prod.js
+++ webpack.config/webpack.prod.js
@@ -1,3 +1,38 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  mode: 'production'
+  mode: 'production',
+  devtool: false,
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'scripts/[name].[contenthash].bundle.js',
+    publicPath: './'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]ss|css)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: false
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
 };
