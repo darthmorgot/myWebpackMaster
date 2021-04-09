a212b812110b0d6565b0155c17036b7e0604a147
darthmorgot <dartmorgot@yandex.ru>
2021-04-09 17:41:26 +1000

    Изменил код в связи с переносом инструкций rules в отдельные файлы

--- webpack.config/webpack.common.js
+++ webpack.config/webpack.common.js
@@ -3,6 +3,11 @@ const path = require('./paths.js');
 const htmlWebpackPlugin = require('./plugins/html-webpack-plugin.js');
 const copyWebpackPlugin = require('./plugins/copy-webpack-plugin.js');
 
+const scriptPreset = require('./presets/script-preset-common.js');
+const stylePreset = require('./presets/style-preset-common.js');
+const imagePreset = require('./presets/image-preset-common.js');
+const fontPreset = require('./presets/font-preset-common.js');
+
 module.exports = {
   entry: {
     main: path.src + '/index.js'
@@ -19,45 +24,10 @@ module.exports = {
   ],
   module: {
     rules: [
-      {
-        test: /\.js$/i,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(s[ac]ss|css)$/i,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader', options: {sourceMap: true}},
-          {loader: 'postcss-loader', options: {sourceMap: true}},
-          {loader: 'sass-loader', options: {sourceMap: true}}
-        ]
-      },
-      {
-        test: /\.(?:ico|gif|jpeg|jpg|png|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|otf|eot|)$/i,
-        type: 'asset/inline',
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts'
-            }
-          }
-        ]
-      }
+      scriptPreset(),
+      stylePreset(),
+      imagePreset(),
+      fontPreset()
     ]
   }
 };
